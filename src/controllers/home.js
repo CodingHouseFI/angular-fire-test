38b715aa22d814b37adca56c510fc734834947fc
cnichols <mellowdious@mail.com>
2015-09-18 11:31:05 -0700

    angfire array adding

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -2,6 +2,14 @@
 
 angular.module('angFireTest')
 
-.controller('homeCtrl', ['$scope', '$state', function($scope, $state) {
+.controller('homeCtrl', function($scope, $state, $firebaseArray) {
+  var ref = new Firebase('https://    YOURFIREBASE    .firebaseio.com/messages');
+
+  $scope.messages = $firebaseArray(ref);
+
+  $scope.addMessage = function(message) {
+    $scope.messages.$add( {text: message} );
+  };
+
   console.log('home loaded');
-}]);
+});
